3ee0cac83088e5d96f4c14c08a7990702fa128a6
phserviceiptv-create <phserviceiptv@gmail.com>
2026-09-22 09:08:59 -0300

    test: combine stable components

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,3 @@
-import M3Site from "../components/M3Site";
-export default function Home(){ return <M3Site />; }
+import LeadForm from "../components/LeadForm";
+import ServiceCard from "../components/ServiceCard";
+export default function Home(){ return <main style={{padding:40,background:"#070707",minHeight:"100vh"}}><ServiceCard name="Teste" description="Teste" icon="Wrench" /><LeadForm /></main>; }
