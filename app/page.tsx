5e87af940d82424c8d1ae1ecdf266576ee3af824
phserviceiptv-create <phserviceiptv@gmail.com>
2026-09-22 09:02:33 -0300

    test: isolate service card import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import LeadForm from "../components/LeadForm";
+import ServiceCard from "../components/ServiceCard";
 
 export default function Home() {
-  return <main style={{ padding: 40, background: "#070707", minHeight: "100vh" }}><LeadForm /></main>;
+  return <main style={{ padding: 40, background: "#070707", minHeight: "100vh" }}><ServiceCard name="Teste" description="Teste" icon="Wrench" /></main>;
 }
